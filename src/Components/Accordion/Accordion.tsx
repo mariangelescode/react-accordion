d3d2f6702908dfb9a0053396081a319f943563c2
Mariangeles <mari_riveraper@live.com.mx>
2023-03-07 01:37:13 -0600

    types ts

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -1,11 +1,11 @@
 import './Accordion.css'
 
-export default function Accordion({
-  question:any,
-  answer:any,
-  active:any,
-  show:any,
-}) {
+export default function Accordion(
+    {question}: {question:string},
+    {answer}: {answer:string},
+    {active}: {active:boolean},
+    {show}: {show:any},
+    ) {
 
     return (
         <div>
